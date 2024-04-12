21d93df9ded483743ea31f13ac80891b86e3a9e7
Shivam <shivamrc189@gmail.com>
2024-04-12 15:38:55 +0530

    added search bar suggestions and responsives ness and other things

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,29 +1,77 @@
 import React, { useState } from 'react';
 import './Form.css';
+import { MdDelete } from 'react-icons/md';
+import data from './MOCK_DATA (3).json';
 
 const Form = ({ getWeather }) => {
-  const [city, setCity] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
+  const [searchWord, setSearchWord] = useState('');
   const [error, setError] = useState('');
+
+  const handleFilter = (e) => {
+    const searchWord = e.target.value;
+    const newFilter = data.filter((value) =>
+      value.name.toLowerCase().includes(searchWord.toLowerCase())
+    );
+    setFilteredData(newFilter);
+    setSearchWord(searchWord);
+  };
+
+  const clearAll = () => {
+    setFilteredData([]);
+    setSearchWord('');
+    setError('');
+  };
+
+  const clickSuggestion = (value) => {
+    setSearchWord(value.name);
+    setFilteredData([]);
+    setError('');
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (city.trim() === '') {
+    if (searchWord.trim() === '') {
       setError('Please enter a city name');
       return;
     }
-    getWeather(city);
-    setCity('');
+    getWeather(searchWord);
+    setSearchWord('');
     setError('');
   };
 
   return (
     <form className="form" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Enter city name"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-      />
+      <div className="input-wrapper">
+        <input
+          type="text"
+          
+          value={searchWord}
+          onChange={handleFilter}
+          placeholder="Enter city name"
+          
+          />
+       
+          
+       {searchWord && ( // Render delete icon only if searchWord is not empty
+          <MdDelete id="delete-icon" onClick={clearAll} />
+        )}
+          
+        
+      </div>
+      {filteredData.length !== 0 && (
+        <div className="data-result">
+          {filteredData.slice(0, 10).map((value, key) => (
+            <div
+              className="data-item"
+              key={key}
+              onClick={() => clickSuggestion(value)}
+            >
+              <p>{value.name}</p>
+            </div>
+          ))}
+        </div>
+      )}
       <button type="submit">Get Weather</button>
       {error && <p className="error-msg">{error}</p>}
     </form>
